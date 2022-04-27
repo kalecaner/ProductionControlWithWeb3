5b2e0dcf32ed37098f77be4898e23bb19364959d
Caner KALE <95261394+kalecaner@users.noreply.github.com>
2022-04-27 23:05:53 +0300

    dev-deploy script added

---
+++ src/scripts/1.dev-deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# exit on first error after this point to avoid redeploying with successful build
+set -e
+
+echo
+echo ---------------------------------------------------------
+echo "Step 1: Build the contract (may take a few seconds)"
+echo ---------------------------------------------------------
+echo
+
+yarn build:release
+
+echo
+echo ---------------------------------------------------------
+echo "Step 2: Deploy the contract"
+echo ---------------------------------------------------------
+echo
+
+near dev-deploy ./build/release/simple.wasm
+
+echo
+echo
+echo ---------------------------------------------------------
+echo "Step 3: Prepare your environment for next steps"
+echo
+echo "(a) find the contract (account) name in the message above"
+echo "    it will look like this: [ Account id: dev-###-### ]"
+echo
+echo "(b) set an environment variable using this account name"
+echo "    see example below (this may not work on Windows)"
+echo
+echo ---------------------------------------------------------
+echo 'export CONTRACT=<dev-123-456>'
+echo
+echo
+echo "Don't forget to initialize the contract"
+echo "near call \$CONTRACT init --accountId \$CONTRACT"
+echo
+echo
+echo ---------------------------------------------------------
+echo
+
+exit 0
